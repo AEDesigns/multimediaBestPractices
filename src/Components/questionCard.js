467f13453524597fb951d29c1519bde245056d84
Andrew Elliott <aedevdesigns@gmail.com>
2022-02-14 09:41:54 -0600

    data rendering in drop down from json

--- src/Components/questionCard.js
+++ src/Components/questionCard.js
@@ -2,9 +2,14 @@ const QuestionCard = (props) => {
     return ( 
         <div>
             {props.questions.questions.map(newQues => {
-                return <div className="card" key={newQues.id}>
-                        <h3>{newQues.question}</h3>
-            </div>
+                return <div className="card">
+                            <h3>{newQues.question}</h3>
+                            <select>
+                                {newQues.choices.map(choices => {
+                                    return <option value={choices.value}>{choices.displayValue}</option>
+                                })}
+                            </select>
+                        </div>
             })}
         </div>
      );
