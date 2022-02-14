8bc749fcc689006cfff2c084056ece431ea01961
Andrew Elliott <aedevdesigns@gmail.com>
2022-02-14 14:03:10 -0600

    set state based on users selection

--- src/Components/questionCard.js
+++ src/Components/questionCard.js
@@ -1,16 +1,27 @@
+import React, { useState } from 'react';
+
 const QuestionCard = (props) => {
+
+    const [state, setState] = useState();
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setState(e.target.value);
+    }
+
     return ( 
         <div>
             {props.questions.questions.map(newQues => {
-                return <div className="card">
+                return <div className="card" key={newQues.id.toString()}>
                             <h3>{newQues.question}</h3>
-                            <select>
+                            <select onChange={(e) => handleChange(e)}>
                                 {newQues.choices.map(choices => {
-                                    return <option value={choices.value}>{choices.displayValue}</option>
+                                    return <option value={choices.value} key={choices.id.toString()}>{choices.displayValue}</option>
                                 })}
                             </select>
                         </div>
             })}
+            {state}
         </div>
      );
 }
